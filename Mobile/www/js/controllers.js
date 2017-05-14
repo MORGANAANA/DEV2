fa93a79777358ca760d019fc37aa40ca17272290
gabriel portal <gdsoportal@restinga.ifrs.edu.br>
2017-05-14 19:16:32 -0300

    Sistema de login no aplicativo

--- Mobile/www/js/controllers.js
+++ Mobile/www/js/controllers.js
@@ -1,7 +1,26 @@
 angular.module('app.controllers', [])
 
-  .controller('loginCtrl', ['$scope', '$stateParams',
-    function ($scope, $stateParams) {
+  .controller('loginCtrl', ['$scope','loginService','$location',
+    function ($scope,loginService,$location) {
+
+      $scope.email = "";
+
+      $scope.senha = "";
+
+      $scope.login = function(){
+        loginService.login($scope.email,$scope.senha,function(result){
+          if(result == true){
+            $location.path('inicial');
+          }else{
+            $location.path('login');
+            console.log("Erro ao logar");
+          }
+        })
+      }
+
+      $scope.logout = function(){
+        loginService.logout();
+      }
 
 
     }])
