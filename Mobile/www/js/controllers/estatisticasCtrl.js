54106eb5259bc00c1bee37676643da5b37a2f177
gabriel portal <gdsoportal@restinga.ifrs.edu.br>
2017-06-06 21:32:06 -0300

    concluindo gerar graficos(sem controle e banco), utilizando factory

--- Mobile/www/js/controllers/estatisticasCtrl.js
+++ Mobile/www/js/controllers/estatisticasCtrl.js
@@ -1,7 +1,7 @@
 angular.module('estatisticasCtrl',['zingchart-angularjs'])
 
-.controller('estatisticasCtrl',['$scope','estatisticaService',
-  function ($scope) {
+.controller('estatisticasCtrl',['$scope','estatisticaService','graficoGeralFactory','graficoUniversidadeFactory','graficoDataFactory',
+  function ($scope,estatisticaService,graficoGeralFactory,graficoUniversidadeFactory,graficoDataFactory) {
 
     var urlServer = '45.76.8.32:80';
 
@@ -22,56 +22,11 @@ angular.module('estatisticasCtrl',['zingchart-angularjs'])
 
     }
 
-    var graficoModel = {
-      globals: {
-        shadow: false,
-        fontFamily: "Comic Sans",
-        fontWeight: "100"
-      },
 
-      type: "pie",
-      backgroundColor: "#fff",
+    $scope.graficoGeral  = graficoGeralFactory;
+    $scope.graficoUniversidade = graficoUniversidadeFactory;
+    $scope.graficoData = graficoDataFactory;
 
-      legend: {
-        layout: "x2",
-        position: "0%",
-        borderColor: "transparent",
-        marker: {
-          borderRadius: 10,
-          borderColor: "transparent"
-        }
-      },
-
-      plot: {
-        'selection-mode':"none",
-        'selected-marker':"none",
-        refAngle: "-90",
-        borderWidth: "0px",
-        valueBox: {
-          placement: "in",
-          text: "%npv %",
-          fontSize: "15px",
-          textAlpha: 1,
-        },
-        animation:{
-          delay:"100000",
-          effect:"3",
-          method:"2",
-          sequence:"1",
-        }
-      },
-      series: [{
-        text: "Acertos",
-        values: [5],
-        backgroundColor: "#5e9732",
-      }, {
-        text: "Erros",
-        values: [10],
-        backgroundColor: "#aa5039"
-      }]
-    };
-
-    $scope.graficoMediaAcertoErros = graficoModel;
 
 
   }])
--- Mobile/www/js/services.js
+++ Mobile/www/js/services.js
@@ -1,7 +1,141 @@
 angular.module('app.services', [])
 
-  .factory('myFactory', [function(){
+  .factory('graficoGeralFactory', ['estatisticaService',function(){
 
+    var grafico = graficoModel = {
+      globals: {
+        shadow: false,
+        fontFamily: "Comic Sans",
+        fontWeight: "100"
+      },
+
+      type: "pie",
+      backgroundColor: "#fff",
+
+      legend: {
+        layout: "x2",
+        position: "0%",
+        borderColor: "transparent",
+        marker: {
+          borderRadius: 10,
+          borderColor: "transparent"
+        }
+      },
+
+      plot: {
+        'selection-mode':"none",
+        'selected-marker':"none",
+        refAngle: "-90",
+        borderWidth: "0px",
+        valueBox: {
+          placement: "in",
+          text: "%npv %",
+          fontSize: "15px",
+          textAlpha: 1,
+        },
+        animation:{
+          delay:"100000",
+          effect:"3",
+          method:"2",
+          sequence:"1",
+        }
+      },
+      series: [{
+        text: "Acertos",
+        values: [5],
+        backgroundColor: "#5e9732",
+      }, {
+        text: "Erros",
+        values: [10],
+        backgroundColor: "#aa5039"
+      }]
+    };
+
+    return grafico;
+  }])
+
+  .factory('graficoUniversidadeFactory', ['estatisticaService',function(){
+
+    var grafico = graficoModel = {
+      globals: {
+        shadow: false,
+        fontFamily: "Comic Sans",
+        fontWeight: "100"
+      },
+
+      type: "pie",
+      backgroundColor: "#fff",
+
+      legend: {
+        layout: "x2",
+        position: "0%",
+        borderColor: "transparent",
+        marker: {
+          borderRadius: 10,
+          borderColor: "transparent"
+        }
+      },
+
+      plot: {
+        'selection-mode':"none",
+        'selected-marker':"none",
+        refAngle: "-90",
+        borderWidth: "0px",
+        valueBox: {
+          placement: "in",
+          text: "%npv %",
+          fontSize: "15px",
+          textAlpha: 1,
+        },
+        animation:{
+          delay:"100000",
+          effect:"3",
+          method:"2",
+          sequence:"1",
+        }
+      },
+      series: [{
+        text: "Acertos",
+        values: [5],
+        backgroundColor: "#5e9732",
+      }, {
+        text: "Erros",
+        values: [10],
+        backgroundColor: "#aa5039"
+      }]
+    };
+
+    return grafico;
+  }])
+
+
+  .factory('graficoDataFactory', ['estatisticaService',function(){
+
+    var grafico = {
+      "type": "line",
+
+      legend: {
+        layout: "x2",
+        position: "0%",
+        borderColor: "transparent",
+        marker: {
+          borderRadius: 10,
+          borderColor: "transparent"
+        }
+      },
+      "plot": {
+        "value-box": {
+          "text": "%node-value"
+        }
+      },
+      "series": [
+        {
+          "values": [1,3,4,6,8,6]
+        }
+      ]
+    }
+
+    return grafico;
   }])
 
   .service('estatisticaService',[function(){
